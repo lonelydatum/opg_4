4f4fe50938f5401d96d92e97c0a0faa163b1a81d
Gar <gar@lonelydatu.com>
2020-02-13 22:02:53 -0500

    one

--- dev/std_300x250/_js/main.js
+++ dev/std_300x250/_js/main.js
@@ -1,26 +1,38 @@
 
-const list_1 = ['.tw_1', '.tw_2']
-const list_2 = ['.tw_3', '.tw_4']
-const list_3 = ['.tw_5', '.tw_6']
+const list_a1 = ['.ta_1', '.ta_2']
+const list_a2 = ['.ta_3', '.ta_4']
+
+const list_b1 = ['.tb_1', '.tb_2']
+const list_b2 = ['.tb_3', '.tb_4']
+const list_b3 = ['.tb_5', '.tb_6', '.tb_7']
+
+const list_b4 = ['.tb_8']
+
 
 
 TweenLite.defaultEase = Power1.easeInOut
 
+const pause = .45
 function start(){
 	const tl = new TimelineMax()
 	tl.set('.frame1', {opacity:1})
-	TweenMax.from('.bg', 13, {y:0})
+	TweenMax.from('.bg', 10, {y:0})
 
-	tl.add("start", "+=.5")
+	tl.add("start", "+=.3")
 	tl.from('.bar', .2, {width:0}, "start")
 
-	tl.add(sentence(list_1), "start")
+	tl.add(sentence(list_a1), "start")
+	tl.add(sentence(list_a2), `+=${pause}`)
 
-	tl.to('.t1', .3, {opacity:0}, "+=2")
+	tl.to('.ta', .4, {opacity:0}, "+=1.2")
 
-	tl.add(sentence(list_2), "+=.1")
+	tl.add(sentence(list_b1), `+=${pause}`)
+	tl.add(sentence(list_b2), `+=${.3}`)
+	tl.add(sentence(list_b3), `+=${pause}`)
+	tl.add(sentence(list_b4), `+=${.6}`)
 
-	tl.add(sentence(list_3), "+=2")
+
+
 
 	tl.set([".t3", ".t2"], {filter:'none'}, 11)
 }
@@ -30,8 +42,7 @@ function sentence(list){
 	const tl = new TimelineMax()
 	list.map((item, i)=>{
 
-		const delay = i * .25
-		console.log(delay);
+		const delay = i * .1
 		tl.from(item, .1, {opacity:0}, `+=${delay}`)		
 	})
 
